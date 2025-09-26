45f0d40f6a6df6435e2a6a1dcb3469c18a2330b9
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-09-26 12:52:27 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
+import ProfileCard from "./components/ProfileCard";
+import "./App.css";
 
-import './App.css'
+export type ProfileCard = {
+  id: number;
+  name: string;
+  designation: string;
+  rate: number;
+  picture: string;
+};
 
 function App() {
-  
+  const profile1: ProfileCard = {
+    id: 1,
+    name: "Fahad Bin Javed",
+    designation: "Website Developer",
+    rate: 20,
+    picture:
+      "https://plus.unsplash.com/premium_photo-1708275672423-837db6d3d700?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDV8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D",
+  };
 
   return (
     <>
-      
-      <div>My Im Fahad</div>
+      <Card
+        id={profile1.id}
+        name={profile1.name}
+        designation={profile1.designation}
+        rate={profile1.rate}
+        picture={profile1.picture}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,19 @@
+type CardProps = {
+  id: number;
+  name: string;
+  designation: string;
+  rate: number;
+  picture: string;
+};
+
+const Card = ({ id, name, designation, rate, picture }: CardProps) => {
+  return(
+
+ <div className="min-h-screen bg-gradient-to-r from-pink-400 via-red-300 to-orange-300" >
+
+ </div>
+
+   );
+};
+
+export default Card;
