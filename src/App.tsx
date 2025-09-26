b893b4f69ac02645fcbdb467a15b1ebcbfa7b8fc
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-09-26 15:18:49 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import ProfileCard from "./components/ProfileCard";
+
 import "./App.css";
+import Card from "./components/ProfileCard";
 
 export type ProfileCard = {
   id: number;
@@ -14,20 +15,75 @@ function App() {
     id: 1,
     name: "Fahad Bin Javed",
     designation: "Website Developer",
-    rate: 20,
+    rate: 34,
     picture:
       "https://plus.unsplash.com/premium_photo-1708275672423-837db6d3d700?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDV8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D",
-  };
+  }
+
+  const profile2: ProfileCard = {
+    id: 1,
+    name: "Fahad Bin Javed",
+    designation: "Website Developer",
+    rate: 20,
+    picture:
+      "https://images.unsplash.com/photo-1602471615287-d733c59b79c4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww",
+  }
+
+  const profile3: ProfileCard = {
+    id: 1,
+    name: "Fahad Bin Javed",
+    designation: "Website Developer",
+    rate: 40,
+    picture:
+      "https://images.unsplash.com/photo-1602471615287-d733c59b79c4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww",
+  }
+
+  const profile4: ProfileCard = {
+    id: 1,
+    name: "Fahad Bin Javed",
+    designation: "Website Developer",
+    rate: 20,
+    picture:
+"https://images.unsplash.com/photo-1602471615287-d733c59b79c4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww"  }
 
   return (
     <>
+    <div className=" min-h-screen min-w-sceen bg-gradient-to-r from-pink-400 via-red-300 to-orange-300 flex items-center justify-center">
+      <div className="flex gap-6">
+
       <Card
         id={profile1.id}
         name={profile1.name}
         designation={profile1.designation}
         rate={profile1.rate}
         picture={profile1.picture}
-      />
+        />
+
+      <Card
+        id={profile2.id}
+        name={profile2.name}
+        designation={profile2.designation}
+        rate={profile2.rate}
+        picture={profile2.picture}
+        />
+
+      <Card
+        id={profile3.id}
+        name={profile3.name}
+        designation={profile3.designation}
+        rate={profile3.rate}
+        picture={profile3.picture}
+        />
+
+      <Card
+        id={profile4.id}
+        name={profile4.name}
+        designation={profile4.designation}
+        rate={profile4.rate}
+        picture={profile4.picture}
+        />
+        </div>
+        </div>
     </>
   );
 }
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -6,10 +6,39 @@ type CardProps = {
   picture: string;
 };
 
-const Card = ({ id, name, designation, rate, picture }: CardProps) => {
+const Card = ({  name, designation, rate, picture }: CardProps) => {
   return(
 
- <div className="min-h-screen bg-gradient-to-r from-pink-400 via-red-300 to-orange-300" >
+    
+
+ <div>
+
+<div className="bg-white h-85 w-60">
+
+
+<div
+  className={`w-16 text-sm rounded-l-2xl pl-2 relative top-7 left-44 ${
+    rate > 30 ? "bg-green-500" : "bg-yellow-500"
+  }`}
+>
+  {rate}/hr
+</div>
+
+
+
+<div className="flex justify-center">
+  <img className="size-24 rounded-full mt-10" src={picture} alt=""/>
+</div>
+<div className="flex flex-col items-center mt-6">
+  <h1 className="text-2xl font-bold">{name}</h1>
+  <hr className="my-1 border-t-3 border-blue-700 w-1/4 mx-auto"/>
+  <h1 className="text-gray-500">{designation}</h1>
+</div>
+<div className="flex justify-center mt-8">
+  <button className="bg-blue-600 text-white text-sm px-16 py-2 rounded-2xl">VIEW PROFILE</button>
+</div>
+</div>
+
 
  </div>
 
